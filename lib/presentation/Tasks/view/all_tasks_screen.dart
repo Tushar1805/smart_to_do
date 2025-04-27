7a411ff0264bc948c95bfe960ea6a76a14e5b9f3
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2025-04-27 19:02:10 +0530

    CI/CD build release

--- lib/presentation/Tasks/view/all_tasks_screen.dart
+++ lib/presentation/Tasks/view/all_tasks_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class AllTasksScreen extends StatelessWidget {
+class AllTasksScreen extends StatefulWidget {
   const AllTasksScreen({super.key});
 
   @override
-  Widget build(final BuildContext context) => Scaffold(
-        body: Center(
-          child: Text('All Tasks Screen'),
-        ),
-      );
+  State<AllTasksScreen> createState() => _AllTasksScreenState();
 }
+
+class _AllTasksScreenState extends State<AllTasksScreen> {
+  var val = 1;
+  @override
+  Widget build(final BuildContext context) {
+    setState(() {
+      val = 2;
+    });
+    return Scaffold(
+      body: SfCalendar(
+        view: CalendarView.month,
+      ),
+    );
+  }
+}
+// class AllTasksScreen extends HookWidget {
+//   const AllTasksScreen({super.key});
+
+//   @override
+//   Widget build(final BuildContext context) => Scaffold(
+//         body: SfCalendar(
+//           view: CalendarView.timelineWeek,
+//         ),
+//       );
+// }
